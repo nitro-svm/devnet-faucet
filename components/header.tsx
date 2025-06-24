411897b835652c5aa0e3860e7544a90572e8351e
Terry <125799816+trlau@users.noreply.github.com>
2025-06-25 07:53:17 +0800

    Update header.tsx
    change image alt text

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     // <div className="top-0 left-0 w-full md:fixed">
     <div className="flex items-center justify-center w-full p-8 mx-auto">
-      <Image src={solaxyLogo} alt="Solana Logo" height={54} />
+      <Image src={solaxyLogo} alt="Solaxy Logo" height={54} />
     </div>
     // </div>
   );
